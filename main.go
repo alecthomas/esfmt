0e4c379bb09f4257893a1197007231128bd4bba5
Alec Thomas <alec@swapoff.org>
2022-01-30 18:30:58 +1100

    Add some more tables.

--- main.go
+++ main.go
@@ -14,41 +14,60 @@ import (
 
 var (
 	cli struct {
-		Source []string `arg:"" help:"Source to format." type:"existingfile"`
+		DebugAST  bool     `help:"Debug dump AST."`
+		DebugType bool     `help:"Debug AST type information."`
+		Source    []string `arg:"" help:"Source to format." type:"existingfile"`
 	}
 	// Token types that trigger a prefix indent.
 	prefixIndentTypes = map[string]bool{
 		"comment": true,
 	}
+	postfixIndentTypes = map[string]bool{
+		";": true,
+		"}": true,
+	}
 	postfixNewlineTypes = map[string]bool{
 		"comment":         true,
 		"statement_block": true,
+		"{":               true,
+		"}":               true,
+		";":               true,
 	}
-	inhibitSpaceTypes = map[string]bool{
-		"property_identifier": true,
-		"member_expression":   true,
+	postfixSpaceTypes = map[string]bool{
+		"interface": true,
+		"class":     true,
+		"const":     true,
+		":":         true,
+	}
+	prefixSpaceTypes = map[string]bool{
+		"{": true,
 	}
 )
 
 func format(w io.Writer, source []byte, indent string, node *sitter.Node) {
 	typ := node.Type()
-	// fmt.Println(typ)
 	isDeclaration := strings.HasSuffix(typ, "_declaration")
 	if prefixIndentTypes[typ] || isDeclaration {
 		fmt.Fprint(w, indent)
 	}
-	if node.ChildCount() == 0 {
-		if !inhibitSpaceTypes[typ] {
-			fmt.Fprint(w, " ")
-		}
-		fmt.Fprintf(w, "%s", source[node.StartByte():node.EndByte()])
+	if prefixSpaceTypes[typ] {
+		fmt.Fprint(w, " ")
 	}
-	if postfixNewlineTypes[typ] || isDeclaration {
-		fmt.Fprintln(w)
+	if node.ChildCount() == 0 {
+		fmt.Fprintf(w, "%s", source[node.StartByte():node.EndByte()])
+		if cli.DebugType {
+			fmt.Fprintf(w, "\033[32m(%s)\033[0m", typ)
+		}
+	}
+	if postfixSpaceTypes[typ] {
+		fmt.Fprint(w, " ")
 	}
 	childCount := int(node.ChildCount())
 	for i := 0; i < childCount; i++ {
 		format(w, source, indent, node.Child(i))
+	}
+	if isDeclaration || postfixNewlineTypes[typ] {
+		fmt.Fprintln(w)
 	}
 }
 
@@ -61,6 +80,9 @@ func main() {
 		content, err := ioutil.ReadFile(source)
 		kctx.FatalIfErrorf(err)
 		tree := parser.Parse(nil, content)
+		if cli.DebugAST {
+			fmt.Fprintln(os.Stderr, tree.RootNode())
+		}
 		// fmt.Println(tree.RootNode())
 		format(os.Stdout, content, "", tree.RootNode())
 	}
